7b3f48bc92e7d46fc0b18d59a89453998a2cb44f
Marius Kalder <147047923+MKalder@users.noreply.github.com>
2026-06-04 14:24:15 +0700

    browser extension

--- node/app.js
+++ node/app.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import { ALLOWED_ORIGINS } from './config/env.js';
 
 const app = express();
+
 app.set('trust proxy', 1);
+
+app.use(cors({
+    origin: ALLOWED_ORIGINS.split(','),
+    credentials: true,
+}));
+
+app.use(cookieParser());
 app.use(express.json());
 app.use('/auth', authRoutes);
 
